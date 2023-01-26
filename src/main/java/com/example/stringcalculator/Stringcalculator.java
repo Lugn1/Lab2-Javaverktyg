e1567d0ba1a48f482ad91accecbee59efd1293e1
William Karlström <wkarlstrom@outlook.com>
2023-01-26 14:34:28 +0100

    Add first test for string calculator

--- src/main/java/com/example/stringcalculator/Stringcalculator.java
+++ src/main/java/com/example/stringcalculator/Stringcalculator.java
@@ -1,4 +1,9 @@
 package com.example.stringcalculator;
 
 public class Stringcalculator {
+    public static int add(String s) {
+        if (s.equals(""))
+            return 0;
+        return -1;
+    }
 }
--- src/test/java/com/example/stringcalculator/StringcalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringcalculatorTest.java
@@ -1,7 +1,15 @@
 package com.example.stringcalculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringcalculatorTest {
 
+
+    @Test
+    void callAddMethodWIthAnEmptyStringShouldReturn0(){
+        assertEquals(0, Stringcalculator.add(""));
+
+    }
 }
